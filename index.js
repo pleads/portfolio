727f821f9134f53666d86c77e917a17e8354eea5
pleads <ainaoluwapelumi06@gmail.com>
2025-04-18 22:11:24 +0100

    change

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
-const activePage = mac.location;
-console.log(activePage);
+function Menu(e){
+    let list = document.querySelector('list');
+
+    e.name ==='menu' ? (e.name = "close", list.classList.add('top-[-80px]'),
+    list.classList.add('opacity-100')): ( e.name = "menu",
+    list.classList.remove('top-[80px]'),list.classList.remove('opacity-100'))
+
+    if (list.classList.contains("hidden")) {
+list.classList.remove("hidden");
+e.name = "close";
+
+} else {
+list.classList.add("hidden");
+e.name = "menu";
+}
+}
